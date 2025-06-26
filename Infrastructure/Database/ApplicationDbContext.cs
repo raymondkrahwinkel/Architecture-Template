933e719ef39ffa12ebc91456bd004000f52098f6
Raymond Krahwinkel <raymond@krahwinkel.nl>
2025-06-26 14:29:46 +0200

    Initial commit

---
+++ Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+    : IdentityDbContext<UserEntity, UserRoleEntity, Guid>(options), IApplicationDbContext
+{
+    public DbSet<TodoItemEntity> TodoItems => Set<TodoItemEntity>();
+}
